d4af8d39fe1b8d78b78c15234886d4aa02cf6587
COOL-SEASONS <info@cool-seasons.com>
2026-05-15 06:15:15 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { supabase } from '@/lib/supabase'
 import { LayoutDashboard,Users,FolderOpen,Wrench,DollarSign,Package,UserCheck,FileText,Bell,ChevronDown,AlertCircle,AlertTriangle,TrendingUp,Building2,Settings,Menu,X,BarChart3,BarChart2 } from 'lucide-react'
 
 import DashboardContent from '@/components/pages/DashboardContent'
+const DashboardAny = DashboardContent as React.ComponentType<any>
 import ClientsPage from '@/components/pages/ClientsPage'
 import ProjectsPage from '@/components/pages/ProjectsPage'
 import TechniciansPage from '@/components/pages/TechniciansPage'
@@ -283,7 +284,7 @@ export default function Home() {
 
   function renderPage() {
     switch(page) {
-      case 'dashboard':         return <DashboardContent onNav={nav}/>
+      case 'dashboard':         return <DashboardAny onNav={nav}/>
       case 'clients':           return <ClientsPage/>
       case 'projects':          return <ProjectsPage/>
       case 'technicians':       return <TechniciansPage/>
